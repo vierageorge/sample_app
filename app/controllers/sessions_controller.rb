2e06b4e57dc79f2c82010b04c1af7685b50eb59c
vierageorge <joviera@gmail.com>
2020-04-02 23:10:21 -0500

    Adds login. Adds mobile responsive. Adds login/no login layout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user&&user.authenticate(params[:session][:password])
       #Log in
+      log_in user
+      redirect_to user
     else
       #Error message
       flash.now[:danger] = 'Invalid email/password combination'
