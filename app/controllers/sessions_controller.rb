0634f8a9f037ec4642c14c6a5072796b0cd48204
vierageorge <joviera@gmail.com>
2020-04-03 18:39:44 -0500

    Adds remember user necesary methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,13 @@
 class SessionsController < ApplicationController
   def new
   end
+
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user&&user.authenticate(params[:session][:password])
       #Log in
       log_in user
+      remember user
       redirect_to user
     else
       #Error message
@@ -13,6 +15,7 @@ class SessionsController < ApplicationController
       render 'new'
     end
   end
+
   def destroy
     log_out
     redirect_to root_url
