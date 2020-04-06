1ebc54259bb2be58d01fb093a3504db4eb57cbd3
vierageorge <joviera@gmail.com>
2020-04-06 07:30:45 -0500

    Creates Account Activation controller. Adds edit route to it

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get     '/contact', to: "static_pages#contact"
   get     '/signup',  to: "users#new"
   resources :users
+  resources :account_activations, only: [:edit]
   get     '/login',   to: 'sessions#new'
   post    '/login',   to: 'sessions#create'
   delete  '/logout',  to: 'sessions#destroy'
