a505ef460b5a47088cf2502588840d0fa089fac8
vierageorge <joviera@gmail.com>
2020-04-06 08:00:13 -0500

    Adds create activation token before creating users. Adds activation status to db seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
-  before_save { self.email = email.downcase }
+  before_save :downcase_email
+  before_create :create_activation_digest
+
   validates :name,  presence: true,
                     length: {maximum: 50}
 
@@ -37,4 +39,14 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+  private
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
