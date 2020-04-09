2e24857dcf9ae1046ecc7e39b08e4c8adcbd42a8
vierageorge <joviera@gmail.com>
2020-04-08 20:16:11 -0500

    Adds password reset controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   root "static_pages#home"
   get     '/help',    to: "static_pages#help"
   get     '/about',   to: "static_pages#about"
@@ -9,6 +11,7 @@ Rails.application.routes.draw do
   get     '/login',   to: 'sessions#new'
   post    '/login',   to: 'sessions#create'
   delete  '/logout',  to: 'sessions#destroy'
+  resources :password_resets,     only: [:new, :create, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
