2fa2ca69b0de627b17a3c119d7c083280b9cca74
vierageorge <joviera@gmail.com>
2020-04-09 20:42:19 -0500

    Adds scope to define order in microposts

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -27,4 +27,8 @@ class MicropostTest < ActiveSupport::TestCase
     assert_not @micropost.valid?
   end
 
+  test "order should be most recent first" do
+    assert_equal microposts(:most_recent), Micropost.first
+  end
+
 end
