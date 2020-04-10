8660effb2ad0a6818befc56b85f947182cd4c0e9
vierageorge <joviera@gmail.com>
2020-04-09 20:39:59 -0500

    Adds hm relation on user, to microposts.

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -6,8 +6,7 @@ class MicropostTest < ActiveSupport::TestCase
   # end
   def setup
     @user = users(:michael)
-    # This code is not idiomatically correct.
-    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+    @micropost = @user.microposts.build(content: "Lorem ipsum")
   end
   test "should be valid" do
     assert @micropost.valid?
