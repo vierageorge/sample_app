4c3a920860496d5153f69ccef5569c3ee60e99be
vierageorge <joviera@gmail.com>
2020-04-11 11:44:20 -0500

    Creates user feed, shows in home for logged in users

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,11 +2,12 @@ class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
 
   def create
-    @micropost = current_user.microposts.build(micropsots_params)
+    @micropost = current_user.microposts.build(microposts_params)
     if @micropost.save
       flash[:success] = 'Micropost created!'
       redirect_to root_url
     else
+      @feed_items = current_user.feed.paginate(page: params[:page])
       render 'static_pages/home'
     end
   end
